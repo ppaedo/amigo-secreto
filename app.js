e7335049340994451969f983b5d254c959fafa4f
Patricio Aedo <aedo.pa@gmail.com>
2025-02-22 21:28:19 -0300

    Función para actualizar lista de amigos

--- app.js
+++ app.js
@@ -3,22 +3,35 @@ let amigos = [];
 
 
 //Agregar ammigos
-function agregarAmigos(){
+function agregarAmigo(){
     const inputAmigos = document.getElementById("amigos");
     const nombreAmigos = inputAmigos.ariaValueMax.trim();
-    
-}   
+  
 
 //Validar que se ingrese un nombre y que no se repitan
     if (amigos === ""){
     alert("Por favor, ingrese un nombre");
     return;
-}else if(amigos.includes(nombreAmigos  )){
+    }else if(amigos.includes(nombreAmigos  )){
     alert(`El nombre ${nombreAmigos} ya está en la lista`);
-}
+    }
 
 //Agregar a nombre a la lista
 amigos.push(nombreAmigos);
 
 //Limpiar campo
 inputAmigos.value = "";
+
+actualizarLista();
+
+}
+function actualizarLista(){
+    const listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    for(let i = 0;i <amigos.length; i++){
+        const li = document.createElement("li");
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+    }
+}
