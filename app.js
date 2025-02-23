46f52b91584f8beba8b5edbc9dbbe848fb452b24
Patricio Aedo <aedo.pa@gmail.com>
2025-02-22 23:38:51 -0300

    Se arregla el if y se agregan funciones

--- app.js
+++ app.js
@@ -1,37 +1,49 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-
-//Agregar ammigos
+//Agregar amigos
 function agregarAmigo(){
-    const inputAmigos = document.getElementById("amigos");
-    const nombreAmigos = inputAmigos.ariaValueMax.trim();
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value.trim();
   
 
 //Validar que se ingrese un nombre y que no se repitan
-    if (amigos === ""){
+    if (nombreAmigo === ""){
     alert("Por favor, ingrese un nombre");
     return;
-    }else if(amigos.includes(nombreAmigos  )){
-    alert(`El nombre ${nombreAmigos} ya está en la lista`);
+    }else if(amigos.includes(nombreAmigo)){
+    alert(`El nombre ${nombreAmigo} ya está en la lista`);
+    return;
     }
-
+   
 //Agregar a nombre a la lista
-amigos.push(nombreAmigos);
+amigos.push(nombreAmigo);
 
 //Limpiar campo
-inputAmigos.value = "";
+inputAmigo.value = "";
 
 actualizarLista();
 
 }
 function actualizarLista(){
-    const listaAmigos = document.getElementById("listaAmigos");
+    let listaAmigos = document.getElementById("listaAmigos");
     listaAmigos.innerHTML = "";
-
-    for(let i = 0;i <amigos.length; i++){
+    for (let i = 0;i < amigos.length; i++){
         const li = document.createElement("li");
         li.textContent = amigos[i];
         listaAmigos.appendChild(li);
     }
+}
+
+function sortearAmigo(){
+    if (amigos.length === 0){
+        alert("No hay amigos en la lista. Favor agregar algunos.")
+        return;
+    }
+    let indiceAleatorio = Math.floor(Math.random()*amigos.length);
+
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>Amigo sorteado: ${amigoSorteado}</li>`;
 }
