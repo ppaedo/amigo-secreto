c47f88ef346b6d103beb8fc59c4b113f77b3accf
Patricio Aedo <aedo.pa@gmail.com>
2025-02-23 12:33:05 -0300

    Se limpia y ordena el código

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = [];
-
+const amigos = [];
 //Agregar amigos
 function agregarAmigo(){
-    let inputAmigo = document.getElementById("amigo");
-    let nombreAmigo = inputAmigo.value.trim();
+    const inputAmigo = document.getElementById("amigo");
+    const nombreAmigo = inputAmigo.value.trim();
   
-
 //Validar que se ingrese un nombre y que no se repitan
     if (nombreAmigo === ""){
     alert("Por favor, ingrese un nombre");
@@ -15,35 +12,28 @@ function agregarAmigo(){
     alert(`El nombre ${nombreAmigo} ya está en la lista`);
     return;
     }
-   
 //Agregar a nombre a la lista
 amigos.push(nombreAmigo);
-
 //Limpiar campo
 inputAmigo.value = "";
 
 actualizarLista();
-
 }
 function actualizarLista(){
-    let listaAmigos = document.getElementById("listaAmigos");
+    const listaAmigos = document.getElementById("listaAmigos");
     listaAmigos.innerHTML = "";
     for (let i = 0;i < amigos.length; i++){
         const li = document.createElement("li");
         li.textContent = amigos[i];
         listaAmigos.appendChild(li);
+        console.log(amigos)
+        console.log(listaAmigos)
     }
 }
-
 function sortearAmigo(){
-    if (amigos.length === 0){
-        alert("No hay amigos en la lista. Favor agregar algunos.")
-        return;
-    }
-    let indiceAleatorio = Math.floor(Math.random()*amigos.length);
 
-    let amigoSorteado = amigos[indiceAleatorio];
-
-    let resultado = document.getElementById("resultado");
-    resultado.innerHTML = `<li>Amigo sorteado: ${amigoSorteado}</li>`;
+    const indiceAleatorio = Math.floor(Math.random()*amigos.length);
+    const amigoSecreto = amigos[indiceAleatorio];
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>El amigo secreto es: ${amigoSecreto}</li>`;
 }
