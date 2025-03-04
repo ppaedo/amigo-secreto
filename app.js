9f1c879a43793cddee33f9e8ef3392ea793d55b0
Patricio Aedo <aedo.pa@gmail.com>
2025-03-04 14:48:48 -0300

    Se agrega EventListener para tecla "Enter"

--- app.js
+++ app.js
@@ -1,45 +1,52 @@
 const amigos = [];
+
 //Agregar amigos
 function agregarAmigo(){
     const inputAmigo = document.getElementById("amigo");
     const nombreAmigo = inputAmigo.value.trim();
   
-//Validar que se ingrese un nombre y que no se repitan
+    //Validar que se ingrese un nombre y que no se repitan
     if (nombreAmigo === ""){
-    alert("Por favor, ingrese un nombre");
-    return;
-    }else if(amigos.includes(nombreAmigo)){
-    alert(`El nombre ${nombreAmigo} ya está en la lista`);
-    return;
+        alert("Por favor, ingrese un nombre");
+        return;
+    } else if(amigos.includes(nombreAmigo)){
+        alert(`El nombre ${nombreAmigo} ya está en la lista`);
+        return;
     }
 
-amigos.push(nombreAmigo);//Agregar a nombre a la lista
+    amigos.push(nombreAmigo); //Agregar a nombre a la lista
 
-inputAmigo.value = ""; //Limpiar campo
-inputAmigo.focus(); //Se establece foco del elemento de entrada
+    inputAmigo.value = ""; //Limpiar campo
+    inputAmigo.focus(); //Se establece foco del elemento de entrada
 
-actualizarLista();
+    actualizarLista();
 }
+
 function actualizarLista(){
     const listaAmigos = document.getElementById("listaAmigos");
     listaAmigos.innerHTML = "";
-    for (let i = 0;i < amigos.length; i++){
+    for (let i = 0; i < amigos.length; i++){
         const li = document.createElement("li");
         li.textContent = amigos[i];
         listaAmigos.appendChild(li);
-        console.log(amigos)
-        console.log(listaAmigos)
+        console.log(amigos);
+        console.log(listaAmigos);
     }
 }
+
 function sortearAmigo(){
-    if (amigos.length===0){
-        alert("No hay amigos para sortear")
+    if (amigos.length === 0){
+        alert("No hay amigos para sortear");
+        return;
     }
-    const indiceAleatorio = Math.floor(Math.random()*amigos.length);
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
     const amigoSecreto = amigos[indiceAleatorio];
     const resultado = document.getElementById("resultado");
     resultado.innerHTML = `<li>El amigo secreto es: ${amigoSecreto}</li>`;
 }
 
-
-  
+document.addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+        agregarAmigo();
+    }
+});
